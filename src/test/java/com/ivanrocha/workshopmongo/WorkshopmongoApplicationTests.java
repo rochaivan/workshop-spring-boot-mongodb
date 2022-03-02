a2c9141c01759102044310931f372531ca023cab
Ivan Rocha <777ivanrocha@gmail.com>
2022-03-02 20:13:15 -0300

    Projeto criado

---
+++ src/test/java/com/ivanrocha/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanrocha.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
